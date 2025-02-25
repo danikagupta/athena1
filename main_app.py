5d0e91ae9e0849660bd2b41398b870aeaa341a92
Danika Gupta <danika@gprof.com>
2025-02-25 13:00:49 -0800

    User and role identification

--- main_app.py
+++ main_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-from firestore_integration import get_google_cloud_credentials, fetch_users, add_user_by_information
+import time
+
+from firestore_integration import get_google_cloud_credentials, fetch_users, add_user_by_information, fetch_users_by_email
 #, show_all_users, show_one_user, update_one_user
 import pandas as pd
 
@@ -9,6 +11,43 @@ def set_up_credentials():
         credentials = get_google_cloud_credentials(jstr)
         st.session_state['credentials']=credentials
     return st.session_state['credentials']
+
+def process_new_user(user_record):
+    email=user_record.get('email')
+    if email.endswith('@pyxeda.ai'):
+        credentials=set_up_credentials()
+        add_user_by_information(credentials, user_record)
+    else:
+        st.error("Non-Pyxeda email found. Please try log in with your Pyxeda.ai account.")
+        time.sleep(5)
+        st.logout()
+
+def initialize_user():
+    name=st.experimental_user.get('name')
+    email=st.experimental_user.get('email')
+
+    st.sidebar.write(f"Name: {name}")
+    st.sidebar.write(f"Email: {email}")
+    st.session_state['name']=name
+    st.session_state['email']=email
+
+    if email is None:
+        #placeholder = st.empty()
+        st.error("No email found. Please try a different log-in.")
+        time.sleep(5)
+        #placeholder.empty()
+        st.logout()
+    else:
+        credentials=set_up_credentials()
+        s=fetch_users_by_email(credentials,email)
+        #st.write(f"Found {len(s)} users with email {email}; s={s}")
+        if len(s)==0: # No user found. We should add .pyxeda.ai users to the system; else reject
+            process_new_user({'name':name,'email':email})
+        else:
+            user_record=s[0]
+            role=user_record.get('role')
+            st.session_state['role']=role
+            st.sidebar.write(f"Role: {role}")
 
 def show_all_users(creds):
     s1=fetch_users(creds)
@@ -38,6 +77,7 @@ def add_one_user(creds):
 
 def main_app():
     creds=set_up_credentials()
+    initialize_user()
     col1,col2,col3=st.tabs(['User List','User Detail','Insert'])
     with col1:
         show_all_users(creds)
